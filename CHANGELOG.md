1ed2e7d9cf921b3f53d2153a5654714ecc0af789
angelos plastropoulos <angelos.plastropoulos@innotecuk.com>
2019-10-10 12:06:56 +0100

    ## 0.0.24 integrate CI attempt (#20)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changelog for platformComms
 
+## 0.0.24 integrate CI attempt (#20)
+
 ## 0.0.23 integrate CI attempt (#19)
 
 ## 0.0.22 integrate CI attempt (#18)
--- include/server.hpp
+++ include/server.hpp
@@ -2,11 +2,11 @@
 #include <string>
 #include <array>
 
-#include "grpc/grpc.h"
-#include "grpcpp/server.h"
-#include "grpcpp/server_builder.h"
-#include "grpcpp/server_context.h"
-#include "grpcpp/security/server_credentials.h"
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
 
 #include "platformCommunications.grpc.pb.h"
 #include "utils.hpp"
